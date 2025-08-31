eef79cb20796e3c68501997da3676a51afbd4f44
ceroideas <49728029+ceroideas@users.noreply.github.com>
2025-08-31 05:18:58 -0400

    Updated VisualizationModal.tsx

--- components/VisualizationModal.tsx
+++ components/VisualizationModal.tsx
@@ -285,7 +285,6 @@ export default function VisualizationModal({ visible, onClose }: VisualizationMo
                 </View>
               </View>
             ))}
-            }
           </View>
 
           {/* Back Button */}
