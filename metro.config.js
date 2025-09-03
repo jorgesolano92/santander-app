2994530bd586bded22c7094c5a3a760f7331c32a
ceroideas <49728029+ceroideas@users.noreply.github.com>
2025-09-03 10:12:28 -0400

    Fix Metro asset resolution configuration

--- metro.config.js
+++ metro.config.js
@@ -2,8 +2,4 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-// Disable updates completely
-config.resolver.resolverMainFields = ['react-native', 'browser', 'main'];
-config.resolver.platforms = ['ios', 'android', 'native', 'web'];
-
 module.exports = config;
