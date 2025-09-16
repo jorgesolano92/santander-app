dcabcb827d4629ad459bab3f732f0ba91013744f
ceroideas <49728029+ceroideas@users.noreply.github.com>
2025-09-16 21:35:10 +0200

    Fix EventEmitter polyfill for web platform

--- metro.config.js
+++ metro.config.js
@@ -2,4 +2,10 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+// Add polyfills for Node.js modules in web environment
+config.resolver.alias = {
+  ...config.resolver.alias,
+  events: 'events',
+};
+
 module.exports = config;
